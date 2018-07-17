f0dec5d64e91baf891ec1ff7f396b42b17960e0e
James Haller <jameshaller27@gmail.com>
2018-07-16 23:50:17 -0400

    Implement basic State and Context classes

--- context.py
+++ context.py
@@ -5,4 +5,24 @@ from state import State
 
 
 class Context:
-    pass
+    """
+    Interface used by the client. Stores the current State.
+    """
+
+    __slots__ = ['_initial_state', '_current_state']
+
+    def __init__(self, initial_state: State):
+        self._current_state = self._initial_state = initial_state
+
+    def run_once(self) -> State or None:
+        return self._current_state.run(self)
+
+    def run(self) -> None:
+        while not self.is_done():
+            self._current_state = self.run_once()
+
+    def is_done(self) -> bool:
+        return self._current_state is None
+
+    def reset(self) -> None:
+        self._current_state = self._initial_state
--- state.py
+++ state.py
@@ -2,7 +2,16 @@
 
 
 from context import Context
+from abc import ABCMeta, abstractmethod
 
 
-class State:
-    pass
+class State(ABCMeta):
+    """
+    Interface for encapsulating the behavior associated with a
+    particular state of the Context.
+    """
+
+    # noinspection PyUnusedLocal
+    @abstractmethod
+    def run(self, context: Context) -> 'State' or None:
+        return None
